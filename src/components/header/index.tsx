2d013b34ef48d3f7d9ffce140c987c23892af4dd
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2024-04-06 00:40:18 +0600

    Add Radix UI dependencies and implement theme toggle

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,46 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "..";
 
 export const Header = () => {
+  const { setTheme } = useTheme();
+
   return (
-    <div className="bg-background">
-      <header className="container py-3">
+    <div className="bg-background border-border border-b shadow">
+      <header className="container py-3 flex items-center justify-between gap-10">
         <Link href="/" className="text-foreground font-bold">
           Fuzzie.
         </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </header>
     </div>
   );
